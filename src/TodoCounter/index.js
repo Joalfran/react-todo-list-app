341d7ead4501d560fdf384e8187c135a779ac128
Joalfran <joalfran.perez@gmail.com>
2026-04-20 06:36:22 -0400

    Adding React Context to TodoCounter component

--- src/TodoCounter/index.js
+++ src/TodoCounter/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import './TodoCounter.css'
+import { TodoContext } from '../TodoContext';
+
 function TodoCounter ({total, completed}) {
+    const { completedTodos, totalTodos } = React.useContext(TodoContext);
     return (
       <h1 className="TodoCounter">
-        Has completado <span>{completed}</span> de <span>{total}</span>
+        Has completado <span>{completedTodos}</span> de <span>{totalTodos}</span>
       </h1>
     )
   }
